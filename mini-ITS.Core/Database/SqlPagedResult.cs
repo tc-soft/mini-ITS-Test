9c99061e1666fdf71a92d09d67d9e3fa3eefdbf4
Tadeusz Ciszewski <office@tc-soft.pl>
2021-06-22 19:18:32 +0200

    feat: Add SqlPagedResult class

---
+++ mini-ITS.Core/Database/SqlPagedResult.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace mini_ITS.Core.Database
+{
+    public class SqlPagedResult<T> : SqlPagedResultBase
+    {
+        public IEnumerable<T> Results { get; set; }
+
+        public static SqlPagedResult<T> From(SqlPagedResultBase pagedResult, IEnumerable<T> results)
+            => new SqlPagedResult<T>
+            {
+                Results = results,
+                CurrentPage = pagedResult.CurrentPage,
+                ResultsPerPage = pagedResult.ResultsPerPage,
+                TotalResults = pagedResult.TotalResults,
+                TotalPages = pagedResult.TotalPages
+            };
+
+        public static SqlPagedResult<T> Create(IEnumerable<T> results, 
+            SqlPagedQueryBase query, int totalResults)
+            => Create(results, query.Page, query.Results, totalResults);
+
+        public static SqlPagedResult<T> Create(IEnumerable<T> results, 
+            int currentPage, int resultsPerPage, int totalResults)
+            {
+                return new SqlPagedResult<T>
+                {
+                    Results = results,
+                    CurrentPage = currentPage,
+                    ResultsPerPage = resultsPerPage,
+                    TotalResults = totalResults,
+                    TotalPages = (int)Math.Ceiling((double)totalResults/resultsPerPage)
+                };
+            }
+    }
+}
