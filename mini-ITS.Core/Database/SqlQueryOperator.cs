5f98129be8926d3509adf915be44426276644c4e
Tadeusz Ciszewski <office@tc-soft.pl>
2021-06-23 13:11:58 +0200

    feat: Add SqlQueryOperator class

---
+++ mini-ITS.Core/Database/SqlQueryOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_ITS.Core.Database
+{
+    public class SqlQueryOperator
+    {
+        public static string Equal = "=";
+        public static string NotEqual = "<>";
+        public static string GreaterThan = ">";
+        public static string LessThan = "<";
+        public static string GreaterThanOrEqual = ">=";
+        public static string LessThanOrEqual = "<=";
+        public static string LIKE = "LIKE";
+    }
+}
