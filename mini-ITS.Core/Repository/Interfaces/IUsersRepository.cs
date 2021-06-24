21bca15954e2a4e10c262831af9ff66d9b73bbd1
Tadeusz Ciszewski <office@tc-soft.pl>
2021-06-24 22:15:08 +0200

    feat: Add UsersRepository class/interface - GetAllAsync(SqlPagedQuery<Users> sqlPagedQuery)

--- mini-ITS.Core/Repository/Interfaces/IUsersRepository.cs
+++ mini-ITS.Core/Repository/Interfaces/IUsersRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using mini_ITS.Core.Database;
 using mini_ITS.Core.Models;
 
 namespace mini_ITS.Core.Repository
@@ -7,5 +8,6 @@ namespace mini_ITS.Core.Repository
     public interface IUsersRepository
     {
         Task<IEnumerable<Users>> GetAllAsync();
+        Task<SqlPagedResult<Users>> GetAllAsync(SqlPagedQuery<Users> sqlPagedQuery);
     }
 }
