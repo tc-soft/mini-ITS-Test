e69dc77bb4952eaa1a65929fb239344d0c982b31
Tadeusz Ciszewski <office@tc-soft.pl>
2021-08-08 22:39:03 +0200

    feat: 20210808

---
+++ mini-ITS.Web/ClientApp/src/_components/AuthProvider.js
@@ -0,0 +1,12 @@
+﻿import { createContext, useContext } from 'react';
+
+const AuthContext = createContext({
+    currentUser: null,
+    handleLogout: (e) => {
+        e.preventDefault();
+    }
+});
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
