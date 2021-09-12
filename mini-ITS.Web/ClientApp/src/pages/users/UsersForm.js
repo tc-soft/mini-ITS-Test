2fa73d05c66286c061823ff403e7ae90add20414
Tadeusz Ciszewski <office@tc-soft.pl>
2021-09-12 21:26:08 +0200

    feat: 20210912 react-hook-form

--- mini-ITS.Web/ClientApp/src/pages/users/UsersForm.js
+++ mini-ITS.Web/ClientApp/src/pages/users/UsersForm.js
@@ -15,7 +15,7 @@ function UsersForm({ history, match }) {
     const [activePassword, setActivePassword] = useState(false);
     const [schema, setSchema] = useState(null);
     const [user, setUser] = useState(null);
-    
+        
     function createUser(values) {
         usersServices.create(values)
             .then((response) => {
@@ -48,25 +48,30 @@ function UsersForm({ history, match }) {
                 console.error('Error: ', error);
             });
     }
-    function initialValues() {
+
+    const { handleSubmit, register, reset, formState: { errors } } = useForm();
+    
+    const onSubmit = (values) => {
+        console.log(values);
+    };
+
+    useEffect(() => {
         if (isEditMode) {
-            setTimeout(() => {
-                usersServices.edit(id)
-                    .then((response) => {
-                        if (response.ok) {
-                            return response.json()
-                                .then((data) => {
-                                    setUser(data);
-                                });
-                        } else {
-                            return response.json()
-                                .then((data) => {
-                                    console.log(data);
-                                })
-                        }
-                    });
-                setActivePassword(false);
-            }, 0);
+            usersServices.edit(id)
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json()
+                            .then((data) => {
+                                setUser(data)
+                            });
+                    } else {
+                        return response.json()
+                            .then((data) => {
+                                console.log(data);
+                            })
+                    }
+                })
+            setActivePassword(false);
         }
         else {
             setUser({
@@ -82,91 +87,131 @@ function UsersForm({ history, match }) {
             });
             setActivePassword(true);
         }
-        return user;
-    }
+    }, [])
 
-    const FormSchema = (activePassword) => Yup.object().shape({
-        login: Yup.string()
-            .required('Login użytkownika jest wymagana'),
-        firstName: Yup.string()
-            .required('Imię użytkownika jest wymagane'),
-        lastName: Yup.string()
-            .required('Nazwisko użytkownika jest wymagane'),
-        department: Yup.string()
-            .required('Dział użytkownika jest wymagany'),
-        email: Yup.string()
-            .email('Email jest niewłaściwy')
-            .required('Email użytkownika jest wymagany'),
-        phone: Yup.string()
-            .required('Telefon użytkownika jest wymagany'),
-        role: Yup.string()
-            .required('Rola użytkownika jest wymagana'),
-        passwordHash:
-            activePassword ? Yup.string().required('Hasło jest wymagane') : Yup.string(),
-        confirmPasswordHash: Yup.string()
-            .when('passwordHash', (password, schema) => {
-                if (password && activePassword) return schema.required('Potwierdzenie hasła jest wymagane');
-            })
-            .oneOf([Yup.ref('passwordHash')], 'Hasła nie są takie same')
-    });
-
-    const { handleSubmit, register, required, errors } = useForm({
-        //defaultValues: initialValues()
-    });
-
-    const onSubmit = (values) => {
-        console.log(values);
-    };
-
-    //useEffect(() => {
-    //    setSchema(FormSchema(activePassword));
-    //}, [activePassword]);
+    useEffect(() => {
+        reset(user)
+    }, [user]);
 
     return (
         <React.Fragment>
+            <h1 className="xxx__title">{isAddMode ? 'Dodaj' : 'Edytuj'}</h1>
+            <br />
             <form onSubmit={handleSubmit(onSubmit)}>
-                <label>First name</label>
+                <label>Login</label><br/>
                 <input
                     type="text"
-                    {...register("First name", { required: true, maxLength: 80 })}
+                    placeholder="Wpisz login"
+                    error={errors.login}
+                    {...register("login", { required: true, maxLength: 80 })}
                 />
-                <label>Last name</label>
+                {errors.login && "Login jest wymagany"}
+                <br />
+
+                <label>Imię</label><br />
                 <input
                     type="text"
-                    {...register("Last name", { required: true, maxLength: 100 })}
+                    placeholder="Wpisz imię"
+                    {...register("firstName", { required: true, maxLength: 80 })}
                 />
-                <label>Email</label>
+                {errors.firstName && "imie jest wymagane"}
+                <p>{errors.firstName?.message}</p><br />
+
+
+                <label>Nazwisko</label><br />
                 <input
                     type="text"
-                    {...register("Email", {
+                    placeholder="Wpisz nazwisko"
+                    {...register("lastName", { required: true, maxLength: 80 })}
+                /><br />
+
+                <label>Dział</label><br />
+                <input
+                    type="text"
+                    placeholder="Wpisz dział"
+                    {...register("department", { required: true, maxLength: 80 })}
+                /><br />
+
+                <label>Email</label><br />
+                <input
+                    type="text"
+                    placeholder="Wpisz email"
+                    {...register("email", {
                         required: true,
                         pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                     })}
-                />
-                <label>Mobile number</label>
+                /><br />
+
+                <label>Telefon</label><br />
                 <input
                     type="tel"
-                    {...register("Mobile number", {
+                    placeholder="Wpisz telefon"
+                    {...register("phone", {
                         required: true,
                         maxLength: 11,
                         minLength: 8
                     })}
-                />
-                <label>Title</label>
+                /><br />
 
-                <label>Are you a developer?</label>
+                <label>Rola</label><br />
                 <input
-                    type="radio"
-                    value="Yes"
-                    {...register("developer", { required: true })}
-                />
-                <input
-                    type="radio"
-                    value="No"
-                    {...register("developer", { required: true })}
-                />
+                    type="text"
+                    placeholder="Wybierz rolę"
+                    {...register("role", { required: true, maxLength: 80 })}
+                /><br /><br /><br />
 
-                <input type="submit" />
+
+
+
+                {isEditMode &&
+                    <div>
+                        <label>
+                        <input
+                            type="checkbox"
+                            defaultChecked={activePassword}
+                            onChange={() => setActivePassword(!activePassword)}
+                        />
+                                Zmiana hasła
+                            </label>
+                    </div>
+                }
+                <br />
+
+
+
+
+                <button type="button" onClick={() => setShowPassword(!showPassword)}>
+                    {showPassword ? "Hide" : "Show"}
+                </button><br />
+
+                <label>Hasło</label><br />
+                <input
+                    type="password"
+                    placeholder="Wpisz hasło"
+                    {...register("passwordHash", { required: true, maxLength: 80 })}
+                /><br />
+
+                <label>Powtórz hasło</label><br />
+                <input
+                    type="password"
+                    placeholder="Powtórz hasło"
+                    {...register("confirmPasswordHash", { required: true, maxLength: 80 })}
+                /><br />
+
+                <div className="">
+                    <button type="submit" className="" disabled={false}>Zapisz</button>
+
+                    <NavLink to={isAddMode ? '.' : '..'} className="">
+                        <button type="button">
+                            Anuluj
+                            </button>
+                    </NavLink>
+
+                    <Link to={isAddMode ? '.' : '..'} className="">
+                        Anuluj 2
+                        </Link>
+                </div>
+
             </form>
         </React.Fragment>
     );
