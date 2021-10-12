09f13f7081f7bf9809b65151d2fa75dbc718565c
Tadeusz Ciszewski <office@tc-soft.pl>
2021-10-12 13:30:17 +0200

    feat: 20211012 using System.Text.Json

--- mini-ITS.Web.Tests/Controllers/UsersControllerTests.cs
+++ mini-ITS.Web.Tests/Controllers/UsersControllerTests.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using mini_ITS.Core.Database;
+using mini_ITS.Core.Models;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using NUnit.Framework;
-using mini_ITS.Core.Database;
-using mini_ITS.Core.Models;
 
 namespace mini_ITS.Web.Tests.Controllers
 {
@@ -19,11 +19,24 @@ namespace mini_ITS.Web.Tests.Controllers
         {
             await LoginAsync();
             var response = await TestClient.GetAsync(ApiRoutes.Users.LoginStatus);
-
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
-            string responseJson = response.Content.ReadAsStringAsync().Result;
-            var respondLogin = JsonConvert.DeserializeObject<RespondLogin>(responseJson);
+            //var response = responseStatusCode.Content.ReadAsStreamAsync().Result;
+            //var response = responseStatusCode.Content.ReadAsStringAsync().Result;
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            //var respondLogin = await response.Content.ReadFromJsonAsync<RespondLogin>();
+            
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var respondLogin = JsonSerializer.Deserialize<RespondLogin>(result, options);
+
+            //var respondLogin = JsonConvert.DeserializeObject<RespondLogin>(response);
+            //var respondLogin = await JsonSerializer.DeserializeAsync<RespondLogin>(result);
 
             TestContext.Out.WriteLine($"Login      : {respondLogin.Login}");
             TestContext.Out.WriteLine($"FirstName  : {respondLogin.FirstName}");
@@ -37,19 +50,21 @@ namespace mini_ITS.Web.Tests.Controllers
         public async Task UsersController_LoginStatusAsync()
         {
             await LoginAsync();
-            var response = await TestClient.GetAsync(ApiRoutes.Users.LoginStatus);
+            var responseStatusCode = await TestClient.GetAsync(ApiRoutes.Users.LoginStatus);
+            Assert.That(responseStatusCode.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            //var response = await TestClient.GetStringAsync(ApiRoutes.Users.LoginStatus);
 
-            string responseJson = response.Content.ReadAsStringAsync().Result;
-            var respondLogin = JsonConvert.DeserializeObject<RespondLogin>(responseJson);
+            var responseJson = await responseStatusCode.Content.ReadAsStreamAsync();
+            //var respondLogin = JsonConvert.DeserializeObject<RespondLogin>(response);
+            //var respondLogin = await JsonSerializer.DeserializeAsync<RespondLogin>(responseJson);
 
-            TestContext.Out.WriteLine($"Login      : {respondLogin.Login}");
-            TestContext.Out.WriteLine($"FirstName  : {respondLogin.FirstName}");
-            TestContext.Out.WriteLine($"LastName   : {respondLogin.LastName}");
-            TestContext.Out.WriteLine($"Department : {respondLogin.Department}");
-            TestContext.Out.WriteLine($"Role       : {respondLogin.Role}");
-            TestContext.Out.WriteLine($"isLogged   : {respondLogin.isLogged}");
+            //TestContext.Out.WriteLine($"Login      : {respondLogin.Login}");
+            //TestContext.Out.WriteLine($"FirstName  : {respondLogin.FirstName}");
+            //TestContext.Out.WriteLine($"LastName   : {respondLogin.LastName}");
+            //TestContext.Out.WriteLine($"Department : {respondLogin.Department}");
+            //TestContext.Out.WriteLine($"Role       : {respondLogin.Role}");
+            //TestContext.Out.WriteLine($"isLogged   : {respondLogin.isLogged}");
         }
 
         [Test]
@@ -83,11 +98,6 @@ namespace mini_ITS.Web.Tests.Controllers
 
             var queryParameters = new Dictionary<string, string>();
 
-            queryParameters.Add("SortColumnName", "Login");
-            queryParameters.Add("SortDirection", "DESC");
-            queryParameters.Add("Page", "1");
-            queryParameters.Add("ResultsPerPage", "5");
-
             sqlPagedQuery.Filter
                 .Select((filter, index) => (filter, index)).ToList()
                 .ForEach((x) =>
@@ -97,30 +107,31 @@ namespace mini_ITS.Web.Tests.Controllers
                     queryParameters.Add($"Filter[{x.index}].Value", x.filter.Value);
                 });
 
-            
-            foreach (var item in sqlPagedQuery.Filter.Select((filter, index) => (filter, index)))
-            {
-                queryParameters.Add($"Filter[{item.index}].Name", item.filter.Name);
-                queryParameters.Add($"Filter[{item.index}].Operator", item.filter.Operator);
-                queryParameters.Add($"Filter[{item.index}].Value", item.filter.Value);
-            }
+            queryParameters.Add("SortColumnName", "Login");
+            queryParameters.Add("SortDirection", "DESC");
+            queryParameters.Add("Page", "1");
+            queryParameters.Add("ResultsPerPage", "5");
 
             var queryString = new FormUrlEncodedContent(queryParameters).ReadAsStringAsync();
-            //var queryString = await dictFormUrlEncoded.ReadAsStringAsync();
 
-            var response = await TestClient.GetAsync($"{ApiRoutes.Users.Index}?{queryString}");
+            var responseStatus = await TestClient.GetAsync($"{ApiRoutes.Users.Index}?{queryString.Result}");
 
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            var response = TestClient.GetStreamAsync($"{ApiRoutes.Users.Index}?{queryString.Result}");
+            
+            //var repositories = await JsonSerializer.DeserializeAsync<SqlPagedResult<UsersDto>>(await response);
+                        
+            Assert.That(responseStatus.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
         }
     }
 
     public class RespondLogin
     {
-        public string Login;
-        public string FirstName;
-        public string LastName;
-        public string Department;
-        public string Role;
-        public bool isLogged;
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+        public bool isLogged { get; set; }
     }
 }
